d0497bb5b9e2f3384c26e68a34fbdc39205828c3
Louis Andrew <louisandrew3@gmail.com>
2021-02-07 00:45:36 +0100

    Done with basic fetching functionalities.

--- src/components/Gifs.vue
+++ src/components/Gifs.vue
@@ -4,9 +4,9 @@
         <p v-bind:class="classes.text">
             Click one of the button below to continue!
         </p>
-        <div class="cat-img">
-            <img v-if="shouldRender" v-bind:src="url" alt="" />
-            <button @click="fetchImg">Fetch</button>
+        <div class="cat-gif">
+            <img v-if="!isLoading" v-bind:src="url" alt="" />
+            <button @click="fetchGif">Fetch</button>
         </div>
     </div>
 </template>
@@ -14,6 +14,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import axios from 'axios';
 
 // eslint-disable-next-line no-unused-vars
 import { Classes } from '@/types';
@@ -26,22 +27,30 @@ const GifsProps = Vue.extend({
 
 @Component
 export default class Gifs extends GifsProps {
-    endpoint = process.env.THE_CAT_ENDPOINT;
-    shouldRender = true;
+    endpoint = process.env.VUE_APP__ENDPOINT;
+    isLoading = false;
+    url = '';
 
-    get url() {
-        return `${this.endpoint}`;
+    created() {
+        this.fetchGif();
     }
 
-    async fetchImg() {
-        fetch(this.endpoint, {
-            method: 'GET',
-            headers: {
-                'x-api-key': process.env.THE_CAT_API_KEY,
-            },
-        })
-            .then(dt => dt.json())
-            .then(s => console.log(s));
+    async fetchGif() {
+        this.isLoading = true;
+        console.log(process.env.VUE_APP_API_KEY);
+        try {
+            const res = await axios(`${this.endpoint}?mime_types=gif`, {
+                headers: {
+                    'x-api-key': process.env.VUE_APP_API_KEY,
+                },
+            });
+            const { data } = await res;
+            this.url = await data[0].url;
+        } catch (e) {
+            console.error(e);
+        } finally {
+            this.isLoading = false;
+        }
     }
 }
 </script>
--- src/components/Pics.vue
+++ src/components/Pics.vue
@@ -4,12 +4,17 @@
         <p v-bind:class="classes.text">
             Click one of the button below to continue!
         </p>
+        <div class="cat-img">
+            <img v-if="!isLoading" v-bind:src="url" alt="" />
+            <button @click="fetchImg">Fetch</button>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import axios from 'axios';
 
 // eslint-disable-next-line no-unused-vars
 import { Classes } from '@/types';
@@ -21,7 +26,33 @@ const PicsProps = Vue.extend({
 });
 
 @Component
-export default class Pics extends PicsProps {}
+export default class Pics extends PicsProps {
+    endpoint = process.env.VUE_APP__ENDPOINT;
+    isLoading = false;
+    url = '';
+
+    created() {
+        this.fetchImg();
+    }
+
+    async fetchImg() {
+        this.isLoading = true;
+        console.log(process.env.VUE_APP_API_KEY);
+        try {
+            const res = await axios(`${this.endpoint}?mime_types=png`, {
+                headers: {
+                    'x-api-key': process.env.VUE_APP_API_KEY,
+                },
+            });
+            const { data } = await res;
+            this.url = await data[0].url;
+        } catch (e) {
+            console.error(e);
+        } finally {
+            this.isLoading = false;
+        }
+    }
+}
 </script>
 
 <style lang="scss" scoped></style>
